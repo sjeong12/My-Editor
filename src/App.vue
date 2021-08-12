8e59aa16589a068593b066c1b45c70a899813e18
sujlee <sujlee@student.42seoul.kr>
2021-08-12 19:57:30 +0900

    add: webpack, babel

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello My Editor',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+h1 {
+	font-size: 24pt;
+}
+</style>
