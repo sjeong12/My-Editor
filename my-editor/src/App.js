990276dd438f34c4c1040756857acba57ef9d479
sujlee <sujlee@student.42seoul.kr>
2021-08-13 15:16:49 +0900

    fix: make react project

---
+++ my-editor/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello My Editor!
+    </div>
+  );
+}
+
+export default App;
--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import Hello from './App.vue';
-
-test('HelloWorld Component', () => {
-	const cmp = new Vue(Hello).$mount();
-	expect(cmp.message).toBe('Hello My Editor');
-});
