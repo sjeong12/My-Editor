4bcb1f0cd83ff689225296f80a4b2d8fa3de24df
sujlee <sujlee@student.42seoul.kr>
2021-09-06 20:30:03 +0900

    feat: create SpinBox components

---
+++ my-editor/src/components/SpinBox.js
@@ -0,0 +1,13 @@
+function SpinBox() {
+  return (
+    <div className="spin-box">
+      <input type="text" value="0"/>
+	  <div class="contorls">
+        <button type="button">+</button>
+        <button type="button">-</button>
+	  </div>
+    </div>
+  );
+}
+
+export default SpinBox;
