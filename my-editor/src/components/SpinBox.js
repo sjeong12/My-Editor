71e4e1d48c26fd8f577914b6d96879ca0dfb0372
sujlee <sujlee@student.42seoul.kr>
2021-09-07 15:27:37 +0900

    FIX: #7 chang class to className in layout

--- my-editor/src/components/SpinBox.js
+++ my-editor/src/components/SpinBox.js
@@ -2,10 +2,10 @@ function SpinBox() {
   return (
     <div className="spin-box">
       <input type="text" value="0"/>
-	  <div class="contorls">
+	    <div className="contorls">
         <button type="button">+</button>
         <button type="button">-</button>
-	  </div>
+	    </div>
     </div>
   );
 }
