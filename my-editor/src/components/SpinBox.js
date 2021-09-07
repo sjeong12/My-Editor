4e07bbf64875a8fbcf36867a2b28cbfceb239ec6
sujlee <sujlee@student.42seoul.kr>
2021-09-07 19:24:04 +0900

    FEAT: set range of value(0 to MAX_SAFE_INTEGER)

--- my-editor/src/components/SpinBox.js
+++ my-editor/src/components/SpinBox.js
@@ -4,15 +4,19 @@ function SpinBox() {
   const [value, setValue] = useState(0);
 
   const handleFocus = e => e.target.select();
-  const handleBlur = () => { if (value === '') setValue(0); }
   const handleChange = e => {
     const onlyNum = e.target.value.replace(/[^0-9]/g, '');
-    setValue(onlyNum);
+    if (Number(onlyNum) < Number.MAX_SAFE_INTEGER)
+      setValue(Number(onlyNum));
+    else
+      setValue(Number.MAX_SAFE_INTEGER);
   }
 
   const onClick = e => {
-    if (e.target.id === "inc-btn") setValue(value => Number(value) + 1);
-    if (e.target.id === "dec-btn") setValue(value => Number(value) - 1);
+    if (e.target.id === "inc-btn" && value < Number.MAX_SAFE_INTEGER)
+      setValue(value => value + 1);
+    if (e.target.id === "dec-btn" && value > 0)
+      setValue(value => value - 1);
   }
 
   return (
@@ -22,7 +26,6 @@ function SpinBox() {
         value={value}
         onChange={handleChange}
         onFocus={handleFocus}
-        onBlur={handleBlur}
       />
 	    <div className="contorls">
         <button id="inc-btn" type="button" onClick={onClick}>+</button>
